48bb25dcd836648e0f74afe8fab7814f6e69faaa
Olga Malkova <Vodeviiil@mail.ru>
2023-05-30 16:11:32 +0300

    11.4 хсет

--- src/main/java/practice/EmailList.java
+++ src/main/java/practice/EmailList.java
@@ -1,19 +1,27 @@
 package practice;
-
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class EmailList {
+    private HashSet<String> todoL = new HashSet<>();
 
     public void add(String email) {
-        // TODO: валидный формат email добавляется, email это строка, она быть может любой
-        // принять решение добавлять аргумент email или нет должен этот метод
+
+        if (isValidEmail(email)) {
+            todoL.add(email.toLowerCase());
+        }else {
+            System.out.println(Main.WRONG_EMAIL_ANSWER);
+        }
+    }
+    private boolean isValidEmail(String email) {
+        return  email.contains("@") && ((email.endsWith(".ru") || email.endsWith(".com")));
     }
 
     public List<String> getSortedEmails() {
-        // TODO: возвращается сортированный список электронных адресов в алфавитном порядке
-        return new ArrayList<>();
+            List<String> sortedList = new ArrayList<>(todoL);
+            Collections.sort(sortedList);
+                return sortedList;
     }
-
 }
--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,33 +1,30 @@
+package practice;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
-    
-    /* TODO:
-        Пример вывода списка Email, после ввода команды LIST в консоль:
-        test@test.com
-        hello@mail.ru
-        - каждый адрес с новой строки
-        - список должен быть отсортирован по алфавиту
-        - email в разных регистрах считается одинаковыми
-           hello@skillbox.ru == HeLLO@SKILLbox.RU
-        - вывод на печать должен быть в нижнем регистре
-           hello@skillbox.ru
-        Пример вывода сообщения об ошибке при неверном формате Email:
-        "Неверный формат email"
-    */
+    public static EmailList todoL = new EmailList();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
         while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("0")) {
+            System.out.println("Введите команду (LIST, ADD, 0)");
+            String command = scanner.nextLine();
+            if (command.equals("0")) {
                 break;
             }
-            
-            //TODO: write code here
-            
+            if (command.equals("ADD")){
+                System.out.println("Введите адрес:");
+                String email = scanner.nextLine();
+                todoL.add(email);
+            }
+            if (command.equals("LIST")){
+                List<String> sortedEmails = todoL.getSortedEmails();
+                for (String email : sortedEmails) {
+                    System.out.println(email);
+                }
+            }
         }
     }
 }
